74d149d4b466dd1eb691b684fa7ca576d68bf4e0
JohnRheyyy <122202655+JohnRheyyy@users.noreply.github.com>
2023-01-08 17:23:05 +0800

    Add files via upload

---
+++ edit.js
@@ -0,0 +1,9 @@
+// const div = document.querySelector('.menu_container');
+
+// window.addEventListener('scroll', function() {
+//   if (window.pageYOffset > div.offsetTop) {
+//     div.classList.add('sticky');
+//   } else {
+//     div.classList.remove('sticky');
+//   }
+// });
